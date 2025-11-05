40eecb05e6d1777f3b809e0a97c3ac3b6616dec3
Joey Santos <joeysantos2007@gmail.com>
2025-11-05 15:48:10 +0800

    Did some things
    - changed the home page to "Under Construction"
    - added unfinished Geodesy page
    - added unfinished Photogrammetry page
    - added unfinished Remote Sensing page
    - added unfinished corrNormTension page
    - added unfinished corrSag page
    - added magDeclComps.ejs file
    - added unfinished meridians page
    - added unfinished taping procedure page
    - added north ejs file
    - edited navbar

--- index.js
+++ index.js
@@ -12,10 +12,14 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // paths
-const frontendPath = path.join(__dirname, "/public");
-const partialsPath = path.join(frontendPath, "/pages partials");
-const learningMainPath = path.join(frontendPath, "/pages Learning Hub");
-const learningSurvPath = path.join(learningMainPath, "/surveying");
+
+const frontendPath = path.join(__dirname, "/public"); // frontend path
+const partialsPath = path.join(frontendPath, "/pages partials"); // partials path
+const learningMainPath = path.join(frontendPath, "/pages Learning Hub"); // main learning hub path
+const learningSurvPath = path.join(learningMainPath, "/surveying"); // surveying path
+const learningCartogrPath = path.join(learningMainPath, "/cartography"); // cartography path
+
+
 
 app.set("view engine", "ejs");
 app.set("views", frontendPath);
@@ -34,23 +38,55 @@ const pgnm = "BSGE Days";
 app.get("/", (req, res) => { res.render("home.ejs", { layout: "layout.ejs" }) });
 
 // Surveying page routes 
+// format: app.get("/surveying/", (req, res) => { res.render(learningSurvPath + "/.ejs") }); //
 {
   app.get("/surveying", (req, res) => { res.render(learningMainPath + "/Surveying.ejs") }); // Main surveying page route
-  app.get("/surveying/pacecalc", (req, res) => { res.render(learningSurvPath + "/pacecalc.ejs") }); // Pacecalc page route
-  app.get("/surveying/corrtemp", (req, res) => { res.render(learningSurvPath + "/corrTemp.ejs") }); // CorrTemp page route
-  app.get("/surveying/corrtension", (req, res) => { res.render(learningSurvPath + "/corrTension.ejs") }); //
-  app.get("/surveying/gencorrtaping", (req, res) => { res.render(learningSurvPath + "/genCorrTaping.ejs") }); // General taping corrections page route
-  app.get("/surveying/transit", (req, res) => { res.render(learningSurvPath + "/transit.ejs") }); // The Engineer's Transit page route
 
+  // corrections page routes
+  {
+    app.get("/surveying/corrnormtension", (req, res) => { res.render(learningSurvPath + "/corrNormTension.ejs") }); // Correction Due to Normal Tension page route
+    app.get("/surveying/corrsag", (req, res) => { res.render(learningSurvPath + "/corrSag.ejs") }); // Correction Due to Sag page route
+    app.get("/surveying/corrtemp", (req, res) => { res.render(learningSurvPath + "/corrTemp.ejs") }); // Correction Due to Temperature page route
+    app.get("/surveying/corrtension", (req, res) => { res.render(learningSurvPath + "/corrTension.ejs") }); // Correction Due to Normal Tension page route
+    app.get("/surveying/gencorrtaping", (req, res) => { res.render(learningSurvPath + "/genCorrTaping.ejs") }); // General taping corrections page route
+  }
+  // about angle page routes
+  {
+    app.get("/surveying/meridians", (req, res) => { res.render(learningSurvPath + "/meridians.ejs") }); //
+    app.get("/surveying/magdeclcomps", (req, res) => { res.render(learningSurvPath + "/magDeclComps.ejs") }); // Magnetic Declination and Compass page route
+    app.get("/surveying/north", (req, res) => { res.render(learningSurvPath + "/theNorth.ejs") }); // The North page route
+    app.get("/surveying/angmeasunts", (req, res) => { res.render(learningSurvPath + "/angularMeasUnits.ejs") }); // Units of Angular Measurement page route
+  }
+  // tools page routes
+  {
+    app.get("/surveying/pacecalc", (req, res) => { res.render(learningSurvPath + "/pacecalc.ejs") }); // Pacecalc page route
+    app.get("/surveying/transit", (req, res) => { res.render(learningSurvPath + "/transit.ejs") }); // The Engineer's Transit page route
+  }
+}
+
+// Cartography page routes
+{
+  app.get("/cartogr", (req, res) => { res.render(learningMainPath + "/Cartography.ejs") }); // Main cartography page route
+}
+
+// Geodesy page route
+{
+  app.get("/geodesy", (req, res) => { res.render(learningMainPath + "/Geodesy.ejs") });
+}
+
+// Photogrammetry page route
+{
+  app.get("/photogram", (req, res) => { res.render(learningMainPath + "/Photogrammetry.ejs") });
 }
 
 
+
+
+
+
+
 // 404 page route
-app.use((req, res) => {
-  res.status(404).render(partialsPath + "/404.ejs", {
-    url: req.originalUrl,
-  });
-});
+app.use((req, res) => { res.status(404).render(partialsPath + "/404.ejs", { url: req.originalUrl, }) });
 
 app.listen(PORT, () => {
   console.log(
