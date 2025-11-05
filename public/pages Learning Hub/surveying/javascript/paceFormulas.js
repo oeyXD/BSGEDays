6f362b910bded36cb00f021bf0e22ebadbc97b21
Joey Santos <joeysantos2007@gmail.com>
2025-11-05 20:10:45 +0800

    Did 2 things
    Altered corrNormTension page and rearranged Surveying page

--- public/pages Learning Hub/surveying/javascript/paceFormulas.js
+++ public/pages Learning Hub/surveying/javascript/paceFormulas.js
@@ -1,7 +1,7 @@
-export let totalDistance = Number(), paces = [], meanPace = Number(), meanPD = Number(), meanRP = Number(), meanPF = Number();
+let totalDistance = Number(), paces = [], meanPace = Number(), meanPD = Number(), meanRP = Number(), meanPF = Number();
 
 // calculate mean No. of Paces
-// document.querySelector("#button1").addEventListener("click", calcMNPace); // adds listener to button 1
+document.querySelector("#button1").addEventListener("click", calcMNPace); // adds listener to button 1
 export function calcMNPace() {
     let total = 0, numOfPaces = 0;
     for (let i = 1; i <= 10; i++) {
@@ -19,7 +19,7 @@ export function calcMNPace() {
 }
 
 // calculate mean Pace Factor
-// document.querySelector("#button2").addEventListener("click", calcMPF); // adds listener to button 2
+document.querySelector("#button2").addEventListener("click", calcMPF); // adds listener to button 2
 export function calcMPF() {
     totalDistance = Number(document.querySelector("body > div:nth-child(3) > p > input[type=number]").value);
     meanPF = (totalDistance / meanPace).toFixed(7);
@@ -27,14 +27,14 @@ export function calcMPF() {
 }
 
 // calculate mean Pace Distance
-// document.querySelector("#button3").addEventListener("click", calcMPD); // adds listener to button 3
+document.querySelector("#button3").addEventListener("click", calcMPD); // adds listener to button 3
 export function calcMPD() {
     meanPD = (meanPF * meanPace).toFixed(7);
     return (document.querySelector("#pd").innerHTML = meanPD);
 }
 
 // calculate mean Relative Precision
-// document.querySelector("#button4").addEventListener("click", calcMRP); // adds listener to button 4
+document.querySelector("#button4").addEventListener("click", calcMRP); // adds listener to button 4
 export function calcMRP() {
     meanRP = ((totalDistance - meanPD) / totalDistance).toFixed(10);
     return (document.querySelector("#rp").innerHTML = meanRP);
